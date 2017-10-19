5039585b5c01922654fda24e0a4f15f02ec7c506
Kizilov <kizilov.yura@gmail.com>
2017-10-19 17:19:58 +0300

    add meteo-review

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    const table = document.querySelector('.meteo-review table');
+    const rows = table.querySelectorAll('tr');
+    var i;
+    for ( i=0; i<rows.length; i++) {
+        rows[i].dataset.id % 2 === 0 ? rows[i].style.backgroundColor = "#fafafa" : ''
+    }
 });
