76ab1ccebf7207c12ab0d91edbb50d03e3a064c3
Kizilov <kizilov.yura@gmail.com>
2017-10-23 17:29:50 +0300

    add archive pdk page

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+
     const table = document.querySelector('.meteo-review table');
-    const rows = table.querySelectorAll('tr');
-    var i;
-    for ( i=0; i<rows.length; i++) {
-        rows[i].dataset.id % 2 === 0 ? rows[i].style.backgroundColor = "#fafafa" : ''
+    if(table) {
+        const rows = table.querySelectorAll('tr');
+        var i;
+        for ( i=0; i<rows.length; i++) {
+            rows[i].dataset.id % 2 === 0 ? rows[i].style.backgroundColor = "#fafafa" : ''
+        }
     }
+
 
 
 
--- app/js/watch.js
+++ app/js/watch.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var watch = document.getElementById("watch");
     function digitalWatch() {
         var date = new Date();
-        var day = date.getDay();
-        var month = date.getMonth();
+        var day = date.getDate();
+        var month = parseInt(date.getMonth() + 1);
         var year = date.getFullYear();
         var hours = date.getHours();
         var minutes = date.getMinutes();
         if (hours < 10) hours = "0" + hours;
         if (minutes < 10) minutes = "0" + minutes;
-        watch.innerHTML = day+ '.' + month + '.' + year + ' ' + hours + ":" + minutes;
+        watch.innerHTML = day + '.' + month + '.' + year + ' ' + hours + ":" + minutes;
     }
     if (watch) {
         function clockStart() {
